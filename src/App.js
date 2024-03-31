8264e0105d0260a0ca5caa7a0b0d6ff98196ab78
Samitha Widanage <samithahansaka@gmail.com>
2024-03-31 23:50:46 +0800

    routes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+const HomePage = () => <div>HomePage</div>;
+const LoginPage = () => <div>Login</div>;
+const PosPage = () => <div>Pos</div>;
+
 function App() {
+  // const location = useLocation();
+  // console.log({ buildNumber});
+  useEffect(() => {
+    // console.log(buildNumber)
+      window.href = '/3000';
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/:buildNumber"><HomePage/></Route>
+          <Route exact path="/:buildNumber/login"><LoginPage/></Route>
+          <Route exact path="/:buildNumber/pos"><PosPage/></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
